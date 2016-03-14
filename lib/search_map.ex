cb13d70bebb8db39e0e74f4d49a20e49bd6fe0d8
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2016-03-14 17:03:02 -0300

    Add module to get all views and process each.

---
+++ lib/search_map.ex
@@ -0,0 +1,12 @@
+defmodule SearchMap do
+  @base_path "/Users/henrique/code/can2/app"
+  @wildcard_views "views/**/*.html.erb"
+
+  def process_views() do
+    Path.join([@base_path, @wildcard_views])
+    |> Path.wildcard
+    |> Stream.map(&HtmlContent.map_text_from_html_file/1)
+    |> Stream.map(&IO.puts/1)
+    |> Stream.run
+  end
+end
