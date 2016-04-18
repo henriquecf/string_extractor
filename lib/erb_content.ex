8a9bef42d508982b4556788ec137ba123648f03e
Ivan <ivan.eng.controle@gmail.com>
2016-04-18 16:15:21 -0300

    Remove strings with just integers or just symbols.

--- lib/erb_content.ex
+++ lib/erb_content.ex
@@ -19,30 +19,33 @@ defmodule ErbContent do
     ~r{\+\s*["'][,\s]+["']\s*\+},
     ~r{["']US["']},
     ~r{".*[\w]=.*"},
-    ~r{"[#]"},
+    ~r{"[#?@]*"},
     ~r{class="<%.*%>"},
-    ~r{"none"}
+    ~r{"none"},
+    ~r{"\d+"}
   ]
   def map_text_from_erb(file_path) do
     file_path
     |> File.stream!
-    |> Stream.map(&(Regex.scan(~r{.*\n}, &1)))
-    |> remove_empty_arrays
-    |> Stream.map(&Enum.at(&1, 0))
-    |> Stream.map(&Enum.at(&1, 0))
     |> Stream.map(&remove_undesired_patterns/1)
     |> Stream.map(&(Regex.scan(~r{<%(.*?)%>}, &1)))
     |> remove_empty_arrays
-    |> Stream.map(&Enum.at(&1, 0))
-    |> Stream.map(&Enum.at(&1, 0))
+    |> Stream.map(&get_position(&1, 0))
+    |> Enum.to_list
+    |> List.flatten
     |> Stream.map(&remove_undesired_patterns/1)
     |> Stream.filter(fn(line) -> Regex.match?(~r{.*["'].*}, line) end)
     |> Stream.map(&(Regex.scan(~r{"(?<text>.*?)"}, &1)))
     |> remove_empty_arrays
-    |> Stream.map(&Enum.at(&1, 0))
-    |> Stream.map(&Enum.at(&1, 1))
+    |> Stream.map(&get_position(&1, 1))
+    |> Enum.to_list
+    |> List.flatten
     |> Enum.map(&Translatable.from_original/1)
     |> TranslatableList.from_list(file_path)
+  end
+
+  defp get_position(list, indice) do
+    Enum.map(list, &Enum.at(&1, indice))
   end
 
   defp remove_undesired_patterns(string) do
